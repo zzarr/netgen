fbde8e585a0c76415a060ee1461e2a9541b6d930
Legi Afatah <legi.gaya@gmail.com>
2024-10-21 10:16:13 +0700

    up

---
+++ resources/views/admin/manajemenadmin.blade.php
@@ -0,0 +1,30 @@
+@extends('admin.layouts.app')
+@section('content')
+<div class="page-header">
+                    <div class="page-title">
+                        <h3>Manajemen Admin</h3>
+                    </div>
+
+                    <nav class="breadcrumb-one" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></a></li>
+                            <!-- <li class="breadcrumb-item"><a href="javascript:void(0);">Starter Kits</a></li> -->
+                            <li class="breadcrumb-item active" aria-current="page"><span>Manajemen Antena</span></li>
+                        </ol>
+                    </nav>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-12 layout-spacing">
+                        <div class="widget-content-area br-4">
+                            <div class="widget-one">
+
+                                <h6>Kick Start you new project with ease!</h6>
+
+                                <p class="">With CORK starter kit, you can begin your work without any hassle. The starter page is highly optimized which gives you freedom to start with minimal code and add only the desired components and plugins required for your project.</p>
+
+                            </div>
+                        </div>
+                    </div>
+    @endsection
