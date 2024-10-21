4cc991a95943fa5608330899c4b1c3944617c99c
Andini2204 <andini0422@gmail.com>
2024-10-21 13:44:00 +0700

    up

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -24,6 +24,14 @@
                             </div>
                         </a>
                     </li>
+                    <li class="menu">
+                        <a href="{{route('manajemen_hubhtb')}}" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rss"><path d="M4 11a9 9 0 0 1 9 9"></path><path d="M4 4a16 16 0 0 1 16 16"></path><circle cx="5" cy="19" r="1"></circle></svg>
+                            <span> Manajemen Hub Htb </span>
+                            </div>
+                        </a>
+                    </li>
 
                     <li class="menu">
                         <a href="javascript:void(0);" aria-expanded="false" class="dropdown-toggle">
@@ -110,20 +118,6 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="menu">
-                        <a href="#submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg>
-                                <span> Manajemen Hub Htb</span>
-                            </div>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                            </div>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="submenu" data-parent="#accordionExample">
-                            <li>
-                                <a href="{{route('manajemen_hubhtb')}}"> Teknisi</a>
-                            </li>
                     <li class="menu">
                         <a href="#submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
