e7301e3f284c3e9d23b78986be8bf8b4faf675b1
Andini2204 <andini0422@gmail.com>
2024-11-01 13:29:54 +0700

    up

--- resources/views/admin/manajemenhubhtb.blade.php
+++ resources/views/admin/manajemenhubhtb.blade.php
@@ -78,7 +78,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel">Edit Data Admin</h5>
+                    <h5 class="modal-title" id="editModalLabel">Edit Hub Htb</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
