709f6f52a014d88a64152e4f5aa7a824e3437fb1
luckydxd <luckydawamuzdzikri@gmail.com>
2024-11-01 13:11:14 +0700

    fix hubhtb

--- app/Http/Controllers/ManajemenHubHtbController.php
+++ app/Http/Controllers/ManajemenHubHtbController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\HubHtb;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
@@ -26,19 +27,19 @@ class ManajemenHubHtbController extends Controller
 
     // Menyimpan data baru ke database
     public function store(Request $request)
-{
-    $request->validate([
-        'nama_alat' => 'required|string|max:255',
-        'alamat' => 'required|string|max:255',
-    ]);
+    {
+        $request->validate([
+            'nama_alat' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+        ]);
 
-    HubHtb::create([
-        'nama_alat' => $request->nama_alat,
-        'alamat' => $request->alamat,
-    ]);
+        HubHtb::create([
+            'nama_alat' => $request->nama_alat,
+            'alamat' => $request->alamat,
+        ]);
 
-    return redirect()->back()->with('success', 'Admin berhasil disimpan');
-}
+        return redirect()->back()->with('success', 'Admin berhasil disimpan');
+    }
 
     // Menampilkan form edit data berdasarkan ID
     public function edit($id)
@@ -48,18 +49,18 @@ class ManajemenHubHtbController extends Controller
     }
 
     public function update(Request $request, $id)
-{
-    $validatedData = $request->validate([
-        'nama_alat' => 'string|max:255',
-        'alamat' => 'string|max:255',
+    {
+        $validatedData = $request->validate([
+            'nama_alat' => 'string|max:255',
+            'alamat' => 'string|max:255',
 
-    ]);
+        ]);
 
-    $hubhtb = User::find($id);
-    $hubhtb->update($validatedData);
+        $hubhtb = HubHtb::find($id);
+        $hubhtb->update($validatedData);
 
-    return response()->json(['success' => 'Data berhasil diupdate']);
-}
+        return response()->json(['success' => 'Data berhasil diupdate']);
+    }
 
 
     // Menghapus data dari database berdasarkan ID
@@ -67,6 +68,6 @@ class ManajemenHubHtbController extends Controller
     {
         $hubhtb = HubHtb::find($id);
         $hubhtb->delete();
-        return response()->json(['success','Data admin berhasil dihapus!']);
+        return response()->json(['success', 'Data admin berhasil dihapus!']);
     }
 }
